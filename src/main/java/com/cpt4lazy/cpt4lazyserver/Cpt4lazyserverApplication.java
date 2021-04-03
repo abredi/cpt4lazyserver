4c490a891a246c1af8ac1fa58076ddbc03f1ad88
Abdulaziz Ali <rediabdulaziz@gmail.com>
2021-04-03 18:15:17 -0500

    init

---
+++ src/main/java/com/cpt4lazy/cpt4lazyserver/Cpt4lazyserverApplication.java
@@ -0,0 +1,13 @@
+package com.cpt4lazy.cpt4lazyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cpt4lazyserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cpt4lazyserverApplication.class, args);
+    }
+
+}
